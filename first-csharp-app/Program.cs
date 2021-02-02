6e00a7eee60d37b949b7b2f6985b5f82d0d8f593
Maggie Dalke <cdalke4@msn.com>
2021-02-01 18:25:50 -0600

    praciting variables, numbers and constants.

--- first-csharp-app/Program.cs
+++ first-csharp-app/Program.cs
@@ -6,12 +6,23 @@ namespace first_csharp_app
     {
         static void Main(string[] args)
         {
+            // Constant practice
+            // cannot modify a constants values. 
+            const float Pi = 3.14f;
+
+
+            // Min an Max values
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+
+            // Variable Practice
             byte number = 2;
             int count = 10;
             float totalPrice = 20.95f;
             char character = 'A';
             string firstName = "Maggie";
-            bool isWorking = true; 
+            bool isWorking = true;
             Console.WriteLine(number);
             Console.WriteLine(count);
             Console.WriteLine(totalPrice);
