43405735f9d4ac12b633ab31222181a44db07846
Maggie Dalke <cdalke4@msn.com>
2021-02-01 18:17:13 -0600

    Add project files.

---
+++ first-csharp-app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace first_csharp_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Maggie";
+            bool isWorking = true; 
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
